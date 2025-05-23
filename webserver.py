46e87ce66ff1fcb7c4fd99b0efd7201972d60700
Siddhant Gahankari <b24cm1058@iitj.ac.in>
2025-05-23 20:52:59 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -9,7 +9,7 @@ def home():
     return "Discord bot ok"
 
 def run():
-    app.run(host = 0.0.0.0 , port = 8080)
+    app.run(host = "0.0.0.0" , port = 8080)
 
 def keep_alive():
     t = Thread(target = run)
